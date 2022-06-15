b54e5f7964c804c17ea715ec25ff797ee7fe010c
Xia MaoFeng <158480192@qq.com>
2022-06-15 14:47:59 +0800

    自定义异常类添加警告日志

--- src/main/java/com/lance/hp/hp_study/common/exception/Asserts.java
+++ src/main/java/com/lance/hp/hp_study/common/exception/Asserts.java
@@ -1,6 +1,7 @@
 package com.lance.hp.hp_study.common.exception;
 
 import com.lance.hp.hp_study.common.api.IErrorCode;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author MaoFengX
@@ -9,8 +10,10 @@ import com.lance.hp.hp_study.common.api.IErrorCode;
  * @Description 断言处理类，用于抛出各种API异常
  * @createTime 2022/05/19 15:15:00
  */
+@Slf4j
 public class Asserts {
     public static void fail(String message) {
+        log.warn(message);
         throw new ApiException(message);
     }
 
